4b0bcf700bdc240e70f1d8bccb339e929f8a7aba
liam <liamamadio@gmail.com>
2023-12-07 21:27:02 -0600

    yo;

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const app = express();
 const port = 4269;
+const PirateBay = require('thepiratebayfixed');
 
-app.get('/', (req, res) => {
-  res.send('hello world');
+
+
+app.get('/', async (req, res) => {
+    console.log('searching');
+    const searchResults = await PirateBay.search('Prometheus', {
+        category: 'video',
+        page: 0,
+        orderBy: 'seeds',
+    })
+
+    console.log(searchResults);
+
+    res.send('ok');
 });
 
 app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
+    console.log(`Listening on port ${port}`);
 });
