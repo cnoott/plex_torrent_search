0860fb06bb598bf0427e02b1d653902169b51fd2
Liam Amadio <liamamadio@gmail.com>
2023-12-10 13:10:33 -0600

    download + progress

--- index.js
+++ index.js
@@ -1,23 +1,118 @@
-const express = require('express');
+import express from 'express';
 const app = express();
+import bodyParser from 'body-parser';
 const port = 4269;
-const PirateBay = require('thepiratebayfixed');
+import PirateBay from 'thepiratebayfixed';
+import WebTorrent from 'webtorrent';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import dotenv from 'dotenv';
+dotenv.config();
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
+const client = new WebTorrent();
+// TODO:
+// - "I'm feeling Lucky"
+// - choose tvshow or movie option
+// - diskspace full
 
+const activeTorrents = {};
+const PATH_TO_MOVIES = process.env.PATH_TO_MOVIES;
+const PATH_TO_TV = process.env.PATH_TO_TV;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/views'));
+app.set('view engine', 'ejs');
 
 app.get('/', async (req, res) => {
+
+  res.render('index');
+});
+
+app.get('/search', async (req, res) => {
+  try {
     console.log('searching');
-    const searchResults = await PirateBay.search('Prometheus', {
-        category: 'video',
-        page: 0,
-        orderBy: 'seeds',
-    })
+    const { query } = req.query;
+    const searchResults = await PirateBay.search(query, {
+      category: 'video',
+      page: 0,
+      orderBy: 'seeds',
+    });
+    console.log(Object.keys(searchResults[0]));
+    res.render(
+      'searchResults',
+      {searchResults: searchResults}
+    );
+  }
+  catch (err) {
+    console.log(err);
+    res.render('error');
+  }
+});
 
-    console.log(searchResults);
+app.get('/choice', async (req, res) => {
+  try {
+    const magnetLink = decodeURIComponent(req.query.magnetLink);
+    console.log(magnetLink);
+    if (activeTorrents[magnetLink]) {
+      res.redirect(
+        `show-progress?magnetLink=${encodeURIComponent(magnetLink)}`
+      );
 
-    res.send('ok');
+      return;
+    }
+    console.log('path', PATH_TO_MOVIES);
+    const torrent = await client.add(magnetLink, {path: PATH_TO_MOVIES});
+    activeTorrents[magnetLink] = { progress: 0 };
+    console.log('torrenting');
+    torrent.on('download', () => {
+      activeTorrents[magnetLink].progress = (torrent.progress * 100).toFixed(2);
+    });
+
+    res.redirect(
+      `show-progress?magnetLink=${encodeURIComponent(magnetLink)}`
+    );
+  } catch (err) {
+    console.log(err);
+    res.render('error');
+  };
+});
+
+app.get('/progress', async (req, res) => {
+  try { 
+    const magnetLink = decodeURIComponent(req.query.magnetLink);
+    console.log(magnetLink);
+    const torrent = activeTorrents[magnetLink];
+    console.log('progress');
+
+    if (torrent) {
+      const percentageComplete = torrent.progress;
+      console.log(percentageComplete);
+      console.log(typeof percentageComplete);
+      if (percentageComplete === 100) {
+        delete activeTorrents[magnetLink];
+        res.redirect('complete');
+      }
+      res.json({percentageComplete});
+    } else {
+      res.status(404).json({ error: 'Torrent not found' });
+    }
+  } catch(err) {
+    console.log(err);
+    res.render('error');
+  }
+});
+
+app.get('/show-progress', async (req, res) => {
+  try {
+    res.render('show-progress');
+  } catch (err) {
+    console.log(err);
+    res.render('error');
+  }
 });
 
 app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
+  console.log(`Listening on port ${port}`);
+
 });
